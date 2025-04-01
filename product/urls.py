519d8b4d9cad9fd399461c957cc97527cc0b0945
Mark <mark.mazur007@gmail.com>
2025-04-01 18:20:16 -0400

    add delete ability

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.product, name='show-products'),
     path('add-product/', views.create_product, name='add-product'),
+    path('delete-product/<int:product_id>/', views.delete_product, name='delete-product')
 ]
