cec085b0e1f56cde8b658ea2b6a87c1247bedee3
Mark <mark.mazur007@gmail.com>
2025-04-01 08:15:00 -0400

    add ability to add products

--- product/views.py
+++ product/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def product(request):
     products = Product.objects.all()
 
     context = {'products': products}
-    return render(request, 'product.html', context=context)
+    return render(request, 'products.html', context)
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show-products')
+    else:
+        form = ProductForm()
+
+    context = {'form': form}
+
+    return render(request, 'product_form.html', context)
